124411f91f7e6bcef30395986277939d99cbcd2f
MingzhenHan <hanmingzhen2002@outlook.com>
2024-11-18 23:49:21 +0800

    delete .gitignore file

--- vllm_npu/build/lib/vllm_npu/model_executor/__init__.py
+++
@@ -1,3 +0,0 @@
-import vllm.model_executor.model_loader as vllm_model_loader
-import vllm_npu.model_executor.ascend_model_loader as ascend_model_loader
-vllm_model_loader.get_architecture_class_name = ascend_model_loader.get_architecture_class_name
